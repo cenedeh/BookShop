c0635885fca21abaddeb25488cff8e4fbc3ff75a
Chuks <chuxszee@hotmail.com>
2021-04-21 23:09:08 +0100

    Implement IRequest and IRequestHandler interface signature for DeleteBooksCommand class

---
+++ BookShop.Application/UseCases/Book/Command/DeleteBooksCommand.cs
@@ -0,0 +1,30 @@
+﻿using BookShop.Application.Interfaces;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BookShop.Application.UseCases.Book.Command
+{
+    public class DeleteBooksCommand : IRequest<bool>
+    {
+        public int BookId { get; set; }
+
+        public class DeleteBooksCommandHandler : IRequestHandler<DeleteBooksCommand, bool>
+        {
+            private readonly IBookStoreRepository _bookStoreRepository;
+            public DeleteBooksCommandHandler(IBookStoreRepository bookStoreRepository)
+            {
+                _bookStoreRepository = bookStoreRepository;
+            }
+            
+            public async Task<bool> Handle(DeleteBooksCommand request, CancellationToken cancellationToken)
+            {
+                var book = await _bookStoreRepository.FindById<Domain.Model.Book>(request.BookId);
+                if (book == null) return false;
+                _bookStoreRepository.DeleteAsync(book);
+                var result = await _bookStoreRepository.SaveChanges();
+                return result;
+            }
+        }
+    }
+}
