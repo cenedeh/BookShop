a977e3236a1e04ef9f620167d8c3181b260786f3
Chuks <chuxszee@hotmail.com>
2021-04-21 14:47:19 +0100

    Created Category class

---
+++ BookShop.Domain/Model/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace BookShop.Domain.Model
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Book Book { get; set; }
+        public int BookId { get; set; }
+    }
+}
