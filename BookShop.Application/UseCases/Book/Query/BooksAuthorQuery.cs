c9f8d0474c75b2069170c01e4de8da48372776b0
Chuks <chuxszee@hotmail.com>
2021-04-21 20:58:27 +0100

    Refactored Handle method in BooksAuthorHandler class

--- BookShop.Application/UseCases/Book/Query/BooksAuthorQuery.cs
+++ BookShop.Application/UseCases/Book/Query/BooksAuthorQuery.cs
@@ -1,7 +1,9 @@
 ﻿using BookShop.Application.Interfaces;
 using BookShop.Application.UseCases.Book.Dto;
+using BookShop.Domain.Model;
 using MediatR;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -23,8 +25,13 @@ namespace BookShop.Application.UseCases.Book.Query
             public async Task<List<GetBooksDto>> Handle(BooksAuthorQuery request, CancellationToken cancellationToken)
             {
                 var booksDtos = new List<GetBooksDto>();
+
                 var books = await _bookStoreRepository.
-                    FindByCondition<Domain.Model.AuthorsBooks>(a => a.AuthorId == request.AuthorId);
+                    FindByCondition<AuthorsBooks>(a => a.AuthorId == request.AuthorId);
+               
+                var authors = await 
+                    _bookStoreRepository.FindByCondition<Author>(a =>
+                        a.Books.Any(b => b.BookId == request.AuthorId));
                 foreach (var book in books)
                 {
                     var getBooksDto = new GetBooksDto
@@ -33,7 +40,13 @@ namespace BookShop.Application.UseCases.Book.Query
                         Published = book.Book.Published,
                         IsbnCode = book.Book.IsbnCode,
                         Title = book.Book.Title,
-                        Category = book.Book.Category.Title
+                        Category = book.Book.Category.Title,
+                        Authors = authors.Select(x=> new GetAuthorsDto
+                        {
+                            Id = x.Id,
+                            FirstName = x.FirstName,
+                            LastName = x.LastName
+                        }).ToList()
                     };
                     booksDtos.Add(getBooksDto);
                 }
