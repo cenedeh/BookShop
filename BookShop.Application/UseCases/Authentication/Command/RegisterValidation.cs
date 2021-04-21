461761c3f6d7255f31b3aa123fbc20204a149bbd
Chuks <chuxszee@hotmail.com>
2021-04-21 17:49:00 +0100

    Added validations to RegisterUserCommand class

---
+++ BookShop.Application/UseCases/Authentication/Command/RegisterValidation.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace BookShop.Application.UseCases.Authentication.Command
+{
+    public class RegisterValidation: AbstractValidator<RegisterUserCommand>
+    {
+        public RegisterValidation()
+        {
+            RuleFor(x => x.Username).NotNull().WithMessage("Username can not be null");
+            RuleFor(x => x.Password).NotNull().WithMessage("Password can not be null");
+        }
+    }
+}
