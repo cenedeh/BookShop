bf5fef58b038624feda26dc706847ab804b87ef4
Chuks <chuxszee@hotmail.com>
2021-04-21 17:59:21 +0100

    Created GetBooksDto class

---
+++ BookShop.Application/UseCases/Book/Dto/GetBooksDto.cs
@@ -0,0 +1,14 @@
+﻿using BookShop.Domain.Model;
+using System;
+
+namespace BookShop.Application.UseCases.Book.Dto
+{
+    public class GetBooksDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string IsbnCode { get; set; }
+        public DateTime Published { get; set; }
+        public Category Category { get; set; }
+    }
+}
