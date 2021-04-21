2135cd48b29e2e2d99e4ff3f7902f12a135fdb87
Chuks <chuxszee@hotmail.com>
2021-04-21 17:10:19 +0100

    Created UserAuthenticationException class

---
+++ BookShop.Application/Exceptions/UserAuthenticationException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookShop.Application.Exceptions
+{
+    public class UserAuthenticationException : Exception
+    {
+        public UserAuthenticationException(string message) : base(message)
+        {
+            
+        }
+    }
+}
