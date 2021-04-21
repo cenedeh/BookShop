6407b3930eb44cc848f5dc4958688b02a859dea5
Chuks <chuxszee@hotmail.com>
2021-04-21 15:42:57 +0100

    Created BookStoreContext class and it's configuration setting and added a Books property to the Author class

--- BookShop.Domain/Model/Author.cs
+++ BookShop.Domain/Model/Author.cs
@@ -1,9 +1,12 @@
-﻿namespace BookShop.Domain.Model
+﻿using System.Collections.Generic;
+
+namespace BookShop.Domain.Model
 {
     public class Author
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public ICollection<AuthorsBooks> Books { get; set; } = new List<AuthorsBooks>();
     }
 }
---
+++ BookShop.Infrastructure/BookStoreContext.cs
@@ -0,0 +1,25 @@
+﻿using BookShop.Domain.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace BookShop.Infrastructure
+{
+    public class BookStoreContext: IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> builder):base(builder) { }
+        public DbSet<Book> Book { get; set; }
+        public DbSet<Author> Author { get; set; }
+        public DbSet<Category> Category { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<AuthorsBooks>().HasKey(k => new { k.AuthorId, k.BookId });
+            builder.Entity<AuthorsBooks>().HasOne(x => x.Book).WithMany(x => x.Books).HasForeignKey(x => x.BookId).OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("FK_AuthorsBooks_Book");
+            builder.Entity<AuthorsBooks>().HasOne(x => x.Author).WithMany(x => x.Books).HasForeignKey(x => x.AuthorId).OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("FK_AuthorsBooks_Author");
+            base.OnModelCreating(builder);
+
+        }
+    }
+}
