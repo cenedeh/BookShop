a02da144e21ea47122bd7ee401729d24be39ab82
Chuks <chuxszee@hotmail.com>
2021-04-21 14:48:57 +0100

    Created AuthorsBooks class

---
+++ BookShop.Domain/Model/AuthorsBooks.cs
@@ -0,0 +1,10 @@
+﻿namespace BookShop.Domain.Model
+{
+    public class AuthorsBooks
+    {
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+    }
+}
