97990fbbae043e9257061cc96acc43bd5e8e6802
Chuks <chuxszee@hotmail.com>
2021-04-21 14:53:57 +0100

    Refactored the Book class, added constructor method and two properties with a Factory class

--- BookShop.Domain/Model/Book.cs
+++ BookShop.Domain/Model/Book.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BookShop.Domain.Model
 {
     public class Book
     {
+        public Book(){ }
+
+        private Book(string title, string isbnCode, Category category){
+            Title = title;
+            IsbnCode = isbnCode;
+            Category = category;
+        }
+
         public int Id { get; set; }
         public string Title { get; set; }
         public string IsbnCode { get; set; }
-        public DateTime Published { get; set; } 
+        public DateTime Published { get; set; }
+        public Category Category { get; set; }
+        public ICollection<AuthorsBooks> Books { get; set; } = new List<AuthorsBooks>();
+        
+        public class Factory
+        {
+            public static Book Instance(string title, string isbnCode, Category category) => 
+                new Book(title, isbnCode, category);
+        }
     }
 }
