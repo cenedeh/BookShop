c96ecd9a541a8d8c6b71a3ea6d85f30f91fc3ce5
Chuks <chuxszee@hotmail.com>
2021-04-21 17:18:34 +0100

    Created BooksValidationException class and added nuget packages

---
+++ BookShop.Application/Exceptions/BooksValidationException.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation.Results;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BookShop.Application.Exceptions
+{
+    public class BooksValidationException : System.Exception
+    {
+        public IDictionary<string, string[]> Failures { get; }
+
+        public BooksValidationException() : base("One or more validation failures have occurred.")
+        {
+            Failures = new Dictionary<string, string[]>();
+        }
+
+        public BooksValidationException(List<ValidationFailure> failures) : this()
+        {
+            var propertyNames = failures.Select(e => e.PropertyName).Distinct();
+
+            foreach (var propertyName in propertyNames)
+            {
+                var propertyFailures = failures.Where(e => e.PropertyName == propertyName).Select(e => e.ErrorMessage).ToArray();
+
+                Failures.Add(propertyName, propertyFailures);
+            }
+        }
+    }
+}
