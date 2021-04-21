818b9799d42dedc91b2dddd6ca6edbc85fbbcf01
Chuks <chuxszee@hotmail.com>
2021-04-21 17:09:12 +0100

    Created NotFoundExceptionException class

---
+++ BookShop.Application/Exceptions/NotFoundExceptionException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookShop.Application.Exceptions
+{
+    public class NotFoundExceptionException : Exception
+    {
+        public NotFoundExceptionException(string message) : base(message)
+        {
+            
+        }
+    }
+}
