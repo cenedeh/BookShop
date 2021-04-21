4fcc24964c36b90b85fc1a780fe6394c795d7063
Chuks <chuxszee@hotmail.com>
2021-04-21 14:40:27 +0100

    Created Book class

---
+++ BookShop.Domain/Model/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookShop.Domain.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string IsbnCode { get; set; }
+        public DateTime Published { get; set; } 
+    }
+}
