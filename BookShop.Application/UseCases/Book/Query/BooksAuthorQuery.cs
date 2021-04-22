3608ccec80d38095be7c2e53bbce8733235ad219
Chuks <chuxszee@hotmail.com>
2021-04-22 08:52:11 +0100

    Added Cache to handle method in BooksAuthorHandler class

--- BookShop.Application/UseCases/Book/Query/BooksAuthorQuery.cs
+++ BookShop.Application/UseCases/Book/Query/BooksAuthorQuery.cs
@@ -2,6 +2,8 @@
 using BookShop.Application.UseCases.Book.Dto;
 using BookShop.Domain.Model;
 using MediatR;
+using Microsoft.Extensions.Caching.Memory;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -16,20 +18,40 @@ namespace BookShop.Application.UseCases.Book.Query
         public class BooksAuthorHandler : IRequestHandler<BooksAuthorQuery, List<GetBooksDto>>
         {
             private readonly IBookStoreRepository _bookStoreRepository;
+            private readonly IMemoryCache _memoryCache;
 
-            public BooksAuthorHandler(IBookStoreRepository bookStoreRepository)
+            public BooksAuthorHandler(IBookStoreRepository bookStoreRepository, IMemoryCache memoryCache)
             {
                 _bookStoreRepository = bookStoreRepository;
+                _memoryCache = memoryCache;
             }
 
             public async Task<List<GetBooksDto>> Handle(BooksAuthorQuery request, CancellationToken cancellationToken)
             {
+                const string cacheKey = "bookAuthorQueryList";
+                if(!_memoryCache.TryGetValue(cacheKey, out List<GetBooksDto> bookQueryDto))
+                {
+                    bookQueryDto = await GetBooksDtos(request);
+                    var cacheExpiryOptions = new MemoryCacheEntryOptions
+                    {
+                        AbsoluteExpiration = DateTime.Now.AddMinutes(5),
+                        Priority = CacheItemPriority.High,
+                        SlidingExpiration = TimeSpan.FromMinutes(2)
+                    };
+                    _memoryCache.Set(cacheKey, bookQueryDto, cacheExpiryOptions);
+                }
+
+                return bookQueryDto;
+                    
+            }
+
+            private async Task<List<GetBooksDto>> GetBooksDtos(BooksAuthorQuery request)
+            {
                 var booksDtos = new List<GetBooksDto>();
 
-                var books = await _bookStoreRepository.
-                    FindByCondition<AuthorsBooks>(a => a.AuthorId == request.AuthorId);
-               
-                var authors = await 
+                var books = await _bookStoreRepository.FindByCondition<AuthorsBooks>(a => a.AuthorId == request.AuthorId);
+
+                var authors = await
                     _bookStoreRepository.FindByCondition<Author>(a =>
                         a.Books.Any(b => b.BookId == request.AuthorId));
                 foreach (var book in books)
@@ -41,7 +63,7 @@ namespace BookShop.Application.UseCases.Book.Query
                         IsbnCode = book.Book.IsbnCode,
                         Title = book.Book.Title,
                         Category = book.Book.Category.Title,
-                        Authors = authors.Select(x=> new GetAuthorsDto
+                        Authors = authors.Select(x => new GetAuthorsDto
                         {
                             Id = x.Id,
                             FirstName = x.FirstName,
@@ -50,7 +72,7 @@ namespace BookShop.Application.UseCases.Book.Query
                     };
                     booksDtos.Add(getBooksDto);
                 }
-                
+
                 return booksDtos;
             }
         }
